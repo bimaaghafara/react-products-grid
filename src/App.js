6d2f3cb93657da9cb76e311728193cf674d7b46d
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-07-03 23:18:55 +0700

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          showLoading: false
+      };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {this.state.showLoading && <div>
+          loading
+        </div>}
+        <div className="container-fluid">
+          <div className="container">
+            <h1>Products Grid</h1>
+            <h2>Ascii Faces</h2>
+          </div>
+          <hr></hr>
+        </div>
+      </Fragment>
+    );
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// styles
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
