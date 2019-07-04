1c95bc58dbe43f371ae459e12b5c0ebfe2ece225
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-07-05 01:28:10 +0700

    add sort, fix loader

--- src/App.js
+++ src/App.js
@@ -8,20 +8,31 @@ import Product from './components/product';
 class App extends Component {
 
   constructor(props) {
-      super(props);
-      this.state = {
-          showLoading: false,
-          products: []
-      };
+    super(props);
+    this.state = {
+        showLoading: false,
+        products: [],
+        sortBy: ''
+    };
+      
+    this.handleSortByChange = this.handleSortByChange.bind(this);
   }
 
   componentDidMount() {
     this.fetchProducts()
   }
 
+  handleSortByChange(event) {
+    const value = event.target.value;
+    this.setState({sortBy: value}, () => {
+      this.fetchProducts()
+    });
+  }
+
   fetchProducts() {
     this.setState({showLoading: true});
-    let url = 'http://localhost:3000/products?_limit=10';
+    let sortBy = this.state.sortBy? `&_sort=${this.state.sortBy}` : '';
+    let url = 'http://localhost:3000/products?_limit=15' + sortBy;
     fetch(url)
       .then(response => response.json())
       .then(products => {
@@ -37,13 +48,29 @@ class App extends Component {
 
     return (
       <Fragment>
-        {state.showLoading && <Loader></Loader>}
+        {this.state.showLoading && <Loader></Loader>}
         <div className="container-fluid">
           <div className="container">
             <h1>Products Grid</h1>
             <h2>Ascii Faces</h2>
           </div>
           <hr></hr>
+        </div>
+
+        <div className="container">
+          <div className="sort-by-wrapper pull-right">
+            <div className="sort-by-label">
+              Sort by:
+            </div>
+            <div className="sort-by-select">
+              <select className="form-control" value={state.sortBy} onChange={this.handleSortByChange}>
+                <option value="">Default</option>
+                <option value="size">Size</option>
+                <option value="price">Price</option>
+                <option value="id">Id</option>
+              </select>
+            </div>
+          </div>
         </div>
 
         <div className="container products-wrapper">
